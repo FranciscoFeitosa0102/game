90068880d178be1feeb30f57cd210f40013e4b2f
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-02-28 17:50:44 -0300

    Ranking.js

--- server/models/Ranking.js
+++ server/models/Ranking.js
@@ -1,10 +1,11 @@
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const RankingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  score: { type: Number, required: true },
+  email: String,
+  score: Number,
 });
 
-module.exports = mongoose.model('Ranking', RankingSchema);
+module.exports = mongoose.model("Ranking", RankingSchema);
+
     
