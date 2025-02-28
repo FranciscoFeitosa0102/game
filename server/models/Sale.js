e52c91556cabb7f1c2a69b3091721566971aeab1
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-02-28 18:19:04 -0300

    Sale.js

--- server/models/Sale.js
+++ server/models/Sale.js
@@ -1,8 +1,21 @@
-const mongoose = require("mongoose");
+const Sale = require('../models/sale');
+const User = require('../models/user');
 
-const SaleSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  amount: Number,
-});
+// Registrar uma venda
+exports.recordSale = async (req, res) => {
+  const { userId, amount } = req.body;
+  try {
+    const sale = new Sale({ userId, amount });
+    await sale.save();
 
-module.exports = mongoose.model("Sale", SaleSchema);
+    // Atualizar o usuário com a quantidade de vendas
+    const user = await User.findById(userId);
+    user.salesCount += 1;
+    user.minutesAvailable = user.salesCount * 5; // Cada venda vale 5 minutos
+    await user.save();
+
+    res.status(201).json(sale);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
