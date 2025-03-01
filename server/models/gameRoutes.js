153426ba40c911287fcf5df7088048600dde3513
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-03-01 08:25:29 -0300

    Delete server/models/gameRoutes.js

--- server/models/gameRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGameTime } = require('../controllers/gameController');
-
-router.post('/time', getGameTime);
-
-module.exports = router;
