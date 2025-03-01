98d022b9d0f4369ebe15e495a9f1ce9468f0fb2a
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-03-01 09:37:49 -0300

    index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,11 @@
-/* src/index.css */
-body {
-  font-family: Arial, sans-serif;
-  background-color: #f4f4f4;
-  margin: 0;
-  padding: 0;
-}
+mport React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
