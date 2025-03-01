613b5176aa53cd7b3d4c8bbf519f77f27d80da8d
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-03-01 09:37:18 -0300

    index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+/* src/index.css */
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+  margin: 0;
+  padding: 0;
+}
