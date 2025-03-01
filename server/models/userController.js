2fe98b3f35e6f1da7bea84d123d7097e9a40aaf0
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-03-01 08:26:31 -0300

    userController.js

--- server/models/userController.js
+++ server/models/userController.js
@@ -1,9 +1,9 @@
-const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-// Cadastrar novo usuário
 exports.createUser = async (req, res) => {
   try {
-    const user = new User(req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({ ...req.body, password: hashedPassword });
     await user.save();
     res.status(201).json(user);
   } catch (err) {
@@ -11,12 +11,11 @@ exports.createUser = async (req, res) => {
   }
 };
 
-// Login de usuário (valida o email e senha)
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email, password });
-    if (!user) {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
     res.status(200).json(user);
